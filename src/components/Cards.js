b116f92627bb978ec13d47cf674e746d8b382660
Veysel <veyselkartalms@gmail.com>
2022-05-26 22:32:07 +0300

    cards section added

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,7 +1,23 @@
+import { useState, useEffect } from "react";
+import cardsData from "api/cards.json";
+
 export default function Cards() {
+
+    const [cards, setCards] = useState([]);
+
+    useEffect(() => {
+        setCards(cardsData);
+    }, []);
+
     return (
-        <div>
-            Cards
+        <div className="grid grid-cols-3 gap-x-4 mt-5">
+            {cards.length && cards.map(card => (
+                <div className="bg-white p-14 rounded-lg shadow-sm flex flex-col items-center text-center">
+                    <img className="w-[150px] h-[150px] mb-6" src={card.image} alt={card.title} />
+                    <h6 className="font-semibold text-lg text-primary-brand-color">{card.title}</h6>
+                    <p className="mt-2 text-sm text-gray-700">{card.description}</p>
+                </div>
+            ))}
         </div>
     )
 }
